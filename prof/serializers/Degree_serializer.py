0b4e29e1a393dfc133bbcf53349729d3dce730eb
Valentina <vbernalb@unal.edu.co>
2020-04-11 19:51:07 -0500

    cambios

--- prof/serializers/Degree_serializer.py
+++ prof/serializers/Degree_serializer.py
@@ -5,12 +5,7 @@ from ..models.Degree_model import Degree
 
 
 class DegreeSerializer(serializers.ModelSerializer):
-    trainer = serializers.SlugRelatedField(
-        read_only=True,
-        slug_field='trainer_id'
-    )
 
     class Meta:
         model = Degree
         fields = ('degree_id', 'degree_name', 'year', 'institution', 'trainer',)
-    read_only_fields = ('trainer', )
